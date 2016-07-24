cc55df0b5054927ba5e9f119f81c1b713e90b992
KikeTF <nepta_etorresf@outlook.com>
2016-07-24 14:57:06 -0500

    Cambios NETF

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Te Amo Fatima Michelle Villalva Cabrera</div>
+                <div class="title">Te Amo Fatima Michelle Villalva Cabrera 2</div>
                 <div class="quote">{{ Inspiring::quote() }}</div>
             </div>
         </div>
