34d20cbccd2a43707a37b0e0a06208992e3585e2
KikeTF <nepta_etorresf@outlook.com>
2016-10-16 16:01:17 -0500

    Boton de crear nuevo con datatable final.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,6 @@
 @section('subtitulo', 'Home')
 
 @section('contenido')
-    <?php
-    $usetable = 0;
-    ?>
     <div>
         <img src="{{ asset('image/logo-ups.png') }}" alt="Universidad Polit&eacute;cnica Salesiana" style="width: 90%; display: block; margin: auto;" >
     </div>
